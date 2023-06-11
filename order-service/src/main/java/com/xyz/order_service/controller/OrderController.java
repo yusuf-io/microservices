30d0fb687f9fdb81dc82bfdc9676f2a33849a0e7
@yusuf.ismailoglu <yusuf.ismailoglu@siemens.com>
2023-06-11 15:28:12 +0300

    integrate resilience4j circuit breaker and add fallback method if dependent service is not available

--- order-service/src/main/java/com/xyz/order_service/controller/OrderController.java
+++ order-service/src/main/java/com/xyz/order_service/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.xyz.order_service.controller;
 
 import com.xyz.order_service.dto.OrderRequest;
 import com.xyz.order_service.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -14,8 +15,24 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    // inventory is the instance name of circuitbreaker defined in application.properties
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
     public String placeOrder(@RequestBody OrderRequest orderRequest) {
         orderService.placeOrder(orderRequest);
         return "Order is placed successfully";
+    }
+
+    /**
+     * fall back function for failures
+     *
+     * @param orderRequest     order request which is same args for placeOrder (aka, same method signature)
+     * @param runtimeException run time exception
+     * @return text that will send back to client when inventory service is problematic
+     */
+    public String fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException) {
+
+        // NOTE: Event if the args are not used inside this method, are still needed in order to satisfy the method signature
+
+        return "Oops! Something went wrong, please order after some time!";
     }
 }
